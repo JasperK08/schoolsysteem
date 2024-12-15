3d3d5a17d3fb76bc0ac372794544eb2da3d17f5d
JasperK08 <jasperkerkhof08@gmail.com>
2024-12-15 21:09:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Rooster</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://github.com/JasperK08/schoolsysteem/blob/main/style.css">
 </head>
 <body>
     <h1>Welkom bij het School Rooster Systeem</h1>
     <nav>
-        <a href="login.php">Inloggen</a>
+        <a href="https://github.com/JasperK08/schoolsysteem/blob/main/login.php">Inloggen</a>
     </nav>
 </body>
 </html>
